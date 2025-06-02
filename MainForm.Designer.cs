2cff65578325feef1eab865b088038c629c66230
MacQuy <macquydev77@gmail.com>
2025-06-02 22:37:09 +0700

    Half complete Main Form

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            this.DraggingSpace = new System.Windows.Forms.PictureBox();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.HomeLabel = new System.Windows.Forms.Label();
+            this.UploadBtn = new System.Windows.Forms.Label();
+            this.DownloadBtn = new System.Windows.Forms.Label();
+            this.DetailsBtn = new System.Windows.Forms.Label();
+            this.UploadPt = new System.Windows.Forms.PictureBox();
+            this.DownloadPt = new System.Windows.Forms.PictureBox();
+            this.DetailsPt = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DraggingSpace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UploadPt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DownloadPt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DetailsPt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DraggingSpace
+            // 
+            this.DraggingSpace.Location = new System.Drawing.Point(0, 0);
+            this.DraggingSpace.Name = "DraggingSpace";
+            this.DraggingSpace.Size = new System.Drawing.Size(1200, 50);
+            this.DraggingSpace.TabIndex = 0;
+            this.DraggingSpace.TabStop = false;
+            this.DraggingSpace.MouseDown += Drag_MouseDown;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Location = new System.Drawing.Point(238, 69);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(962, 730);
+            this.MainPanel.TabIndex = 1;
+            // 
+            // HomeLabel
+            // 
+            this.HomeLabel.AutoSize = true;
+            this.HomeLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HomeLabel.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.HomeLabel.Location = new System.Drawing.Point(12, 69);
+            this.HomeLabel.Name = "HomeLabel";
+            this.HomeLabel.Size = new System.Drawing.Size(108, 40);
+            this.HomeLabel.TabIndex = 2;
+            this.HomeLabel.Text = "Home";
+            // 
+            // UploadBtn
+            // 
+            this.UploadBtn.AutoSize = true;
+            this.UploadBtn.Font = new System.Drawing.Font("Microsoft YaHei UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UploadBtn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.UploadBtn.Location = new System.Drawing.Point(51, 141);
+            this.UploadBtn.Name = "UploadBtn";
+            this.UploadBtn.Size = new System.Drawing.Size(126, 40);
+            this.UploadBtn.TabIndex = 3;
+            this.UploadBtn.Text = "Upload";
+            this.UploadBtn.Click += new System.EventHandler(this.UploadBtn_Click);
+            // 
+            // DownloadBtn
+            // 
+            this.DownloadBtn.AutoSize = true;
+            this.DownloadBtn.Font = new System.Drawing.Font("Microsoft YaHei UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownloadBtn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.DownloadBtn.Location = new System.Drawing.Point(51, 215);
+            this.DownloadBtn.Name = "DownloadBtn";
+            this.DownloadBtn.Size = new System.Drawing.Size(172, 40);
+            this.DownloadBtn.TabIndex = 4;
+            this.DownloadBtn.Text = "Download";
+            this.DownloadBtn.Click += new System.EventHandler(this.DownloadBtn_Click);
+            // 
+            // DetailsBtn
+            // 
+            this.DetailsBtn.AutoSize = true;
+            this.DetailsBtn.Font = new System.Drawing.Font("Microsoft YaHei UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DetailsBtn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.DetailsBtn.Location = new System.Drawing.Point(51, 287);
+            this.DetailsBtn.Name = "DetailsBtn";
+            this.DetailsBtn.Size = new System.Drawing.Size(120, 40);
+            this.DetailsBtn.TabIndex = 5;
+            this.DetailsBtn.Text = "Details";
+            this.DetailsBtn.Click += new System.EventHandler(this.DetailsBtn_Click);
+            // 
+            // UploadPt
+            // 
+            this.UploadPt.Image = global::Crypto.Properties.Resources.pointer;
+            this.UploadPt.Location = new System.Drawing.Point(12, 141);
+            this.UploadPt.Name = "UploadPt";
+            this.UploadPt.Size = new System.Drawing.Size(40, 40);
+            this.UploadPt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.UploadPt.TabIndex = 6;
+            this.UploadPt.TabStop = false;
+            // 
+            // DownloadPt
+            // 
+            this.DownloadPt.Image = global::Crypto.Properties.Resources.pointer;
+            this.DownloadPt.Location = new System.Drawing.Point(12, 215);
+            this.DownloadPt.Name = "DownloadPt";
+            this.DownloadPt.Size = new System.Drawing.Size(40, 40);
+            this.DownloadPt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.DownloadPt.TabIndex = 7;
+            this.DownloadPt.TabStop = false;
+            this.DownloadPt.Visible = false;
+            // 
+            // DetailsPt
+            // 
+            this.DetailsPt.Image = global::Crypto.Properties.Resources.pointer;
+            this.DetailsPt.Location = new System.Drawing.Point(12, 287);
+            this.DetailsPt.Name = "DetailsPt";
+            this.DetailsPt.Size = new System.Drawing.Size(40, 40);
+            this.DetailsPt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.DetailsPt.TabIndex = 8;
+            this.DetailsPt.TabStop = false;
+            this.DetailsPt.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(1200, 800);
+            this.Controls.Add(this.DetailsPt);
+            this.Controls.Add(this.DownloadPt);
+            this.Controls.Add(this.UploadPt);
+            this.Controls.Add(this.DetailsBtn);
+            this.Controls.Add(this.DownloadBtn);
+            this.Controls.Add(this.UploadBtn);
+            this.Controls.Add(this.HomeLabel);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.DraggingSpace);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            ((System.ComponentModel.ISupportInitialize)(this.DraggingSpace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UploadPt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DownloadPt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DetailsPt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox DraggingSpace;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Label HomeLabel;
+        private System.Windows.Forms.Label UploadBtn;
+        private System.Windows.Forms.Label DownloadBtn;
+        private System.Windows.Forms.Label DetailsBtn;
+        private System.Windows.Forms.PictureBox UploadPt;
+        private System.Windows.Forms.PictureBox DownloadPt;
+        private System.Windows.Forms.PictureBox DetailsPt;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,45 @@ namespace Crypto
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern void ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        private static extern void SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HTCAPTION = 0x2;
+
+        private void Drag_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void UploadBtn_Click(object sender, EventArgs e)
+        {
+            UploadPt.Visible = true;
+            DownloadPt.Visible = false;
+            DetailsPt.Visible = false;
+        }
+
+        private void DownloadBtn_Click(object sender, EventArgs e)
+        {
+            UploadPt.Visible = false;
+            DownloadPt.Visible = true;
+            DetailsPt.Visible = false;
+        }
+
+        private void DetailsBtn_Click(object sender, EventArgs e)
+        {
+            UploadPt.Visible = false;
+            DownloadPt.Visible = false;
+            DetailsPt.Visible = true;
         }
     }
 }
