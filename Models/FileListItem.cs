61a65bcd61bad568f096fcc4ca7bf1d395c3a930
MacQuy <macquydev77@gmail.com>
2025-06-11 00:39:16 +0700

    Add file model

---
+++ Models/FileListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crypto.Models
+{
+    internal class FileListItem
+    {
+        public string file_id { get; set; }
+        public string original_filename { get; set; }
+        public long size { get; set; }
+        public string uploaded_at { get; set; }
+    }
+}
